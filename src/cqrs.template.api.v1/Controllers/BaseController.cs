4de03ca53853ed802f2e77efe3152b195a6dda15
lincolntx <lincolnsf98@gmail.com>
2020-08-30 15:37:28 -0300

    fix BaseController.cs

--- src/cqrs.template.api.v1/Controllers/BaseController.cs
+++ src/cqrs.template.api.v1/Controllers/BaseController.cs
@@ -1,15 +1,45 @@
-﻿﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
+ using cqrs.template.api.v1.Filters;
+ using cqrs.template.domain.Exceptions;
+ using MediatR;
+ using Microsoft.AspNetCore.Mvc;
 
 namespace cqrs.template.api.v1.Controllers
 {
 	[Route("your-project-name/[controller]")]
+	[ServiceFilter(typeof(GlobalExceptionFilterAttribute))]
 	public class BaseController : Controller
 	{
-		
+		private readonly ExceptionNotificationHandler _notifications;
+
+		protected IEnumerable<ExceptionNotification> Notifications => _notifications.GetNotifications();
+
+		protected BaseController(INotificationHandler<ExceptionNotification> notifications)
+		{
+			_notifications = (ExceptionNotificationHandler) notifications;
+		}
+
+		protected bool IsValidOperation()
+		{
+			return (!_notifications.HasNotifications());
+		}
+
+		protected new IActionResult Response(int statusCode, object result = null)
+		{
+			if (IsValidOperation())
+			{
+				return StatusCode(statusCode, new
+				{
+					success = true,
+					data = result
+				});
+			}
+
+			return BadRequest(new
+			{
+				success = false,
+				errors = _notifications.GetNotifications()
+			});
+		}
 	}
 }
